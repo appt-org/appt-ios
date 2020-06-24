75591c5a4dd19e11175d4c6ed26fd83fb2288b00
Jan Jaap de Groot <info@janjaap.com>
2020-06-24 15:18:25 +0200

    Implement information screen

---
+++ Appt/ViewControllers/InformationViewController.swift
@@ -0,0 +1,68 @@
+//
+//  InformationViewController.swift
+//  Appt
+//
+//  Created by Jan Jaap de Groot on 24/06/2020.
+//  Copyright © 2020 Abra B.V. All rights reserved.
+//
+
+import UIKit
+
+class InformationViewController: ViewController {
+
+    @IBOutlet private var tableView: UITableView!
+    
+    private var subjects = [
+        "Over de app",
+        "Algemene voorwaarden",
+        "Privacybeleid"
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set-up UITableView
+        tableView.registerNib(TitleTableViewCell.self)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.keyboardDismissMode = .onDrag
+        tableView.tableFooterView = UIView(frame: .zero)
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.refreshControl = refreshControl
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension InformationViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return subjects.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.cell(TitleTableViewCell.self, at: indexPath)
+        
+        let subject = subjects[indexPath.row]
+        cell.setup(subject)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return "Mede mogelijk gemaakt door het SIDN fonds"
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension InformationViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
