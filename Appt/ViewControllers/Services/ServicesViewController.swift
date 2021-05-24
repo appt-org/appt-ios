e40eeb410aa4337db5ee6dc750b2005d46d7386f
Yulian Baranetskyy <ybaranetskyy@5prosoftware.com>
2021-05-24 19:30:25 +0300

    Rearranged tabs; Added ServicesViewController

---
+++ Appt/ViewControllers/Services/ServicesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ServicesViewController.swift
+//  Appt
+//
+//  Created by Yulian Baranetskyy on 24.05.2021.
+//  Copyright © 2021 Stichting Appt. All rights reserved.
+//
+
+import UIKit
+
+class ServicesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "services_vc_title".localized
+    }
+}
