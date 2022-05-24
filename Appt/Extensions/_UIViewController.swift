22a05d098808e5aa770299dc13f17cb0108e1f0b
Jan Jaap de Groot <info@janjaap.com>
2022-05-24 17:41:57 +0200

    Handle non-http links better

--- Appt/Extensions/_UIViewController.swift
+++ Appt/Extensions/_UIViewController.swift
@@ -12,11 +12,21 @@ import SafariServices
 extension UIViewController {
 
     func openWebsite(_ urlString: String, delegate: SFSafariViewControllerDelegate? = nil) {
-        guard let url = URL(string: urlString) else { return }
+        guard let url = URL(string: urlString) else {
+            return
+        }
         openWebsite(url)
     }
     
     func openWebsite(_ url: URL, delegate: SFSafariViewControllerDelegate? = nil) {
+        guard url.absoluteString.starts(with: "http") else {
+            if url.absoluteString.starts(with: "mailto") {
+                // TODO: Show list of e-mail clients
+            }
+            UIApplication.shared.open(url)
+            return
+        }
+        
         let configuration = SFSafariViewController.Configuration()
         configuration.barCollapsingEnabled = false
         configuration.entersReaderIfAvailable = false
